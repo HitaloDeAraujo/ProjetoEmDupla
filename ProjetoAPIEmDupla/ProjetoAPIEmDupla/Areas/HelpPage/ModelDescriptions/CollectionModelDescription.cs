94248dbf9d056d41d9e5930e6f2b5e438ead6f33
HitaloDeAraujo <hitalofilipe.hitalofilipe@gmail.com>
2017-01-20 12:18:35 -0200

    Adicionando

---
+++ ProjetoAPIEmDupla/ProjetoAPIEmDupla/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ProjetoAPIEmDupla.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ProjetoAPIEmDupla/ProjetoAPIEmDupla/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProjetoAPIEmDupla.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
