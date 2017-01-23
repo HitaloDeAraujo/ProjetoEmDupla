a9cf4587b1e72b4bea34b09f89d300ae44b3d0f7
João Pedro <joao.mago@drlavatudo.com>
2017-01-23 11:16:13 -0200

    Adicionando Cliente API

---
+++ ProjetoAPIEmDupla/ProjetoAPIEmDupla/Controllers/API/ClienteController.cs
@@ -0,0 +1,26 @@
+﻿using ProjetoAPIEmDupla.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ProjetoAPIEmDupla.Controllers.API
+{
+    public class ClienteController : ApiController
+    {
+        private bdDuplaEntities bd = new bdDuplaEntities();
+
+        public IEnumerable<OrdemServico> Get(string id)
+        {
+            bd.Configuration.ProxyCreationEnabled = false;
+
+            var cliente = bd.OrdemServico.Include("Cliente").Where(m => m.IdCliente == id).ToList();
+            bd.OrdemServico.Include("ServicoPorOS").Where(m => m.IdCliente == id).ToList();
+            bd.ServicoPorOS.Include("Servico").Where(m => m.IdSPO.ToString() == id).ToList();
+
+            return cliente;
+        }
+    }
+}
