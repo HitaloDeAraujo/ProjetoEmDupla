455c58e2064d20e7b0228e763ff1e3d255efdff4
HitaloDeAraujo <hitalofilipe.hitalofilipe@gmail.com>
2017-01-23 13:45:57 -0200

    Atualizando ui OS

---
+++ ProjetoAPIEmDupla/ProjetoAPIEmDupla/Controllers/UI/UIController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoAPIEmDupla.Controllers.UI
+{
+    public class UIController : Controller
+    {
+        // GET: UI
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OS(int id)
+        {
+            return View();
+        }
+    }
+}
